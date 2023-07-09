2cf4c7133b9f9945c73c2e12e6ae98eef5b24b3d
Ronald Ferdinand <mail.ronaldferdinand@gmail.com>
2023-07-10 00:59:24 +0700

    v0.1

---
+++ fe-vue3/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-4 bg-gray-600 shadow-sm text-center text-white">
+      VUE3 - TAILWIND CSS
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  body {
+    background: lightgrey !important;
+  }
+</style>
